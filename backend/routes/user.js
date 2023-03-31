7ab74583ac464281992c1dae7e2afc0042a34135
CAVALCANTE NOA p2107540 <noa.cavalcante@etu.univ-lyon1.fr>
2023-03-31 11:06:09 +0200

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,8 +5,26 @@ const router = express.Router()
 
 router.get('/api/users/:userId', async (req, res) => {
   const { userId } = req.params
-  let user = User.findByPk(userId)
-  res.status(200).send(user)
+  let user = await User.findByPk(userId, {
+    include: [{
+      model: Product,
+      as: 'products',
+      attributes: ['id', 'name', 'description', 'category', 'originalPrice', 'pictureUrl', 'endDate']
+    }, {
+      model: Bid,
+      as: 'bids',
+      attributes: ['id', 'price', 'date'],
+      include: [{
+        model: Product,
+        as: 'product',
+        attributes: ['id', 'name']
+      }]
+    }]
+  })
+  if (user)
+    res.status(200).json(user)
+  else
+    res.status(404).json({ "error": "User not found" })
 })
 
 export default router
