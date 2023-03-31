fb04cc4ba4524d195de345f2f5489cef6c49b7f4
CAVALCANTE NOA p2107540 <noa.cavalcante@etu.univ-lyon1.fr>
2023-03-31 09:32:02 +0200

    Update authors.js

--- frontend/meta/authors.js
+++ frontend/meta/authors.js
@@ -1,5 +1,5 @@
 // Noms et numéros d'étudiant
-export const authors = ["John Doe (12345678)", "Jane Doe (12345678)"];
+export const authors = ["Noa Cavalcante (p2107540)", "Lilian Ouraha (p2110297)"];
 
 // Groupe
-export const group = 0;
+export const group = 3;
