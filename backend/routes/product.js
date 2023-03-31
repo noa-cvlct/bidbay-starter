c2dc73e5623fa006d7b7e65bcf6b613cb247c287
CAVALCANTE NOA p2107540 <noa.cavalcante@etu.univ-lyon1.fr>
2023-03-31 11:06:14 +0200

    Update product.js

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,25 +6,56 @@ import { getDetails } from '../validators/index.js'
 const router = express.Router()
 
 router.get('/api/products', async (req, res, next) => {
-  res.status(600).send()
+  let products = await Product.findAll({
+    attributes: ['id', 'name', 'description', 'category', 'originalPrice', 'pictureUrl', 'endDate'],
+    include: [{
+      model: User,
+      as: 'seller',
+      attributes: ['id', 'username']
+    }, {
+      model: Bid,
+      as: 'bids',
+      attributes: ['id', 'price', 'date']
+    }]
+  });
+  res.status(200).json(products)
 })
 
 router.get('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  const { productId } = req.params
+  let product = await Product.findByPk(productId, {
+    attributes: ['id', 'name', 'description', 'category', 'originalPrice', 'pictureUrl', 'endDate'],
+    include: [{
+      model: User,
+      as: 'seller',
+      attributes: ['id', 'username']
+    }, {
+      model: Bid,
+      as: 'bids',
+      attributes: ['id', 'price', 'date']
+    }]
+  })
+  if (product)
+    res.status(200).json(product)
+  else
+    res.status(404).json({ "error": "Product not found" })
 })
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
 router.post('/api/products', (req, res) => {
-  res.status(600).send()
+  res.status(201).send()
+
+  // authMiddleware
+  // req.user.id
 })
 
 router.put('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  res.status(200).send()
 })
 
 router.delete('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  res.status(204).send()
 })
 
 export default router
